614ef6bd995863801305238aab6728bac47a0e1e
lovyan03 <42724151+lovyan03@users.noreply.github.com>
2021-12-16 18:14:18 +0900

    update examples

--- examples/graph/graph.ino
+++ examples/graph/graph.ino
@@ -8,13 +8,19 @@
 
 #include <esp_log.h>
 
-static float* rawdata;
-static size_t rawdata_len;
-static int rawdata_idx = 0;
+static float* tempdata_buf;
+static size_t tempdata_count;
+static size_t tempdata_idx = 0;
 static int graph_height;
-static int draw_y_offset;
+static int graph_y_offset;
 static float min_temp;
 static float max_temp;
+
+static constexpr size_t avg_count = 20;
+static constexpr size_t delay_msec = 50;
+
+static float avg_buf[avg_count];
+static size_t avg_index = 0;
 
 M5GFX display;
 UNIT_KMeter sensor;
@@ -25,7 +31,8 @@ void setup(void)
 {
   display.begin();
 
-  Wire.begin(SDA, SCL, 400000);
+  Wire.begin(SDA, SCL, 400000L);
+  //Wire.begin(21, 22, 400000L);
 
   sensor.begin();
 
@@ -39,9 +46,9 @@ void setup(void)
   display.setTextColor((uint32_t)~display.getBaseColor(), display.getBaseColor());
   display.setTextDatum(textdatum_t::top_right);
   display.setTextPadding(display.textWidth("1888.88", &fonts::Font4));
-  draw_y_offset = display.fontHeight(&fonts::Font4);
+  graph_y_offset = display.fontHeight(&fonts::Font4);
 
-  graph_height = display.height() - draw_y_offset;
+  graph_height = display.height() - graph_y_offset;
 
   for (int i = 0; i < 2; ++i)
   {
@@ -51,80 +58,96 @@ void setup(void)
     canvas[i].setTextColor(TFT_LIGHTGRAY);
   }
 
-  rawdata_len = display.width();
-  rawdata = (float*)malloc(rawdata_len * sizeof(float));
-//  memset(rawdata, 0, rawdata_len * sizeof(float));
+  tempdata_count = display.width() + avg_count + 1;
+  tempdata_buf = (float*)malloc(tempdata_count * sizeof(float));
 
   float temperature = sensor.getTemperature();
   min_temp = max_temp = temperature;
-  for (int i = 0; i < rawdata_len; ++i)
+  for (size_t i = 0; i < tempdata_count; ++i)
   {
-    rawdata[i] = temperature;
+    tempdata_buf[i] = temperature;
+  }
+  for (size_t i = 0; i < avg_count; ++i)
+  {
+    avg_buf[i] = temperature;
   }
 }
+
+static int vertline_idx = 0;
 
 void drawGraph(void)
 {
   float min_t = INT16_MAX;
   float max_t = INT16_MIN;
-  for (int i = 0; i < rawdata_len; ++i)
+  for (int i = 0; i < tempdata_count; ++i)
   {
-    float value = rawdata[i];
-    if (min_t > value) { min_t = value; }
-    if (max_t < value) { max_t = value; }
+    float t = tempdata_buf[i];
+    if (min_t > t) { min_t = t; }
+    if (max_t < t) { max_t = t; }
   }
   float diff = (max_t - min_t) + 1;
   min_temp = (min_temp + min_t - (diff / 20)) / 2;
   max_temp = (max_temp + max_t + (diff / 20)) / 2;
 
-  int k16 = (graph_height << 16) / (max_temp - min_temp);
+  float magnify = (float)graph_height / (max_temp - min_temp);
 
-  static constexpr int steps[] = { 1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000 };
+  static constexpr int steps[] = { 1, 2, 5, 10, 20, 50, 100, 200, 500, INT_MAX };
   int step_index = 0;
-  while (((k16 * steps[step_index]) >> 16) < 10) { ++step_index; }
+  while (magnify * steps[step_index] < 10) { ++step_index; }
   int step = steps[step_index];
   bool flip = 0;
 
   canvas[flip].clear(display.getBaseColor());
 
-  int v = ((int)min_temp / step) * step;
+  int gauge = ((int)min_temp / step) * step;
   do
   {
-    canvas[flip].drawPixel(0, graph_height - ((int)((v - min_temp ) * k16) >> 16), TFT_DARKGRAY);
-  } while (max_temp > (v += step));
+    canvas[flip].drawPixel(0, graph_height - ((gauge - min_temp ) * magnify), TFT_DARKGRAY);
+  } while (max_temp > (gauge += step));
 
   size_t buffer_len = canvas[flip].bufferLength();
   auto buffer = (uint8_t*)alloca(buffer_len);
   memcpy(buffer, canvas[flip].getBuffer(), buffer_len);
 
-  int push_x = 2;
-  int x_next = rawdata_idx;
+  int drawindex = tempdata_idx;
+  int draw_x = -1;
+  int y0 = 0;
 
-  int y0 = graph_height - ((int)((rawdata[x_next] - min_temp) * k16) >> 16);
+  if (++vertline_idx >= 20) { vertline_idx = 0; }
+  int vidx = vertline_idx;
+
   display.startWrite();
   do
   {
-    memcpy(canvas[flip].getBuffer(), buffer, buffer_len);
-    int x = x_next;
-    x_next = (x + 1) % rawdata_len;
+    if (++vidx >= 20)
+    {
+      vidx = 0;
+      memset(canvas[flip].getBuffer(), 0x55, buffer_len);
+    }
+    else
+    {
+      memcpy(canvas[flip].getBuffer(), buffer, buffer_len);
+    }
 
     int y1 = y0;
-    y0 = graph_height - ((int)((rawdata[x_next] - min_temp) * k16) >> 16);
-    if (display.width() - push_x < 24)
+    y0 = graph_height - ((tempdata_buf[drawindex] - min_temp) * magnify);
+    if (++drawindex >= tempdata_count) { drawindex = 0; }
+
+    if (display.width() - draw_x < 24)
     {
-      v = ((int)min_temp / step) * step;
+      gauge = ((int)min_temp / step) * step;
       do
       {
-        canvas[flip].drawNumber((int)v, display.width() - push_x, graph_height - ((int)((v - min_temp ) * k16) >> 16));
-      } while (max_temp > (v += step));
+        canvas[flip].drawNumber((int)gauge, display.width() - draw_x, graph_height - ((gauge - min_temp) * magnify));
+      } while (max_temp > (gauge += step));
     }
 
     int y_min = y0, y_max = y1;
-    if (y_min > y_max) std::swap(y_min, y_max);
+    if (y_min > y_max) { std::swap(y_min, y_max); }
     canvas[flip].drawFastVLine(0, y_min, y_max - y_min + 1, ~display.getBaseColor());
-    canvas[flip].pushSprite(&display, push_x, draw_y_offset);
+    canvas[flip].pushSprite(&display, draw_x, graph_y_offset);
     flip = !flip;
-  } while (++push_x < rawdata_len);
+  } while (++draw_x < display.width());
   display.endWrite();
 }
 
@@ -132,10 +155,18 @@ void loop(void)
 {
   float temperature = sensor.getTemperature();
 
-  rawdata[rawdata_idx] = temperature;
-  if (++rawdata_idx >= rawdata_len)
+  avg_buf[avg_index] = temperature;
+  if (++avg_index >= avg_count) { avg_index = 0; }
+  float avg_temp = 0;
+  for (size_t i = 0; i < avg_count; ++i)
   {
-    rawdata_idx = 0;
+    avg_temp += avg_buf[i];
+  }
+
+  tempdata_buf[tempdata_idx] = avg_temp / avg_count;
+  if (++tempdata_idx >= tempdata_count)
+  {
+    tempdata_idx = 0;
   }
 
   drawGraph();
@@ -143,11 +174,10 @@ void loop(void)
 
   static uint32_t prev_msec;
   uint32_t msec = millis();
-  if (msec - prev_msec < 100)
+  if (msec - prev_msec < delay_msec)
   {
-    ESP_LOGI("main", "%2.2f", temperature);
-    m5gfx::delay(100 - (msec - prev_msec));
+    ESP_LOGI("loop", "%2.2f", temperature);
+    m5gfx::delay(delay_msec - (msec - prev_msec));
   }
   prev_msec = msec;
-//*/
 }
--- examples/simple/simple.ino
+++ examples/simple/simple.ino
@@ -1,23 +1,25 @@
 
 #include <UNIT_KMeter.h>
 #include <M5GFX.h>
+#include <esp_log.h>
 
 M5GFX display;
-
 UNIT_KMeter sensor;
 
 void setup(void)
 {
   display.begin();
+
+  Wire.begin(SDA, SCL, 400000L);
+  //Wire.begin(21, 22, 400000L);
+
+  sensor.begin();
+
   display.setTextSize(2);
   if (display.width() < display.height())
   {
     display.setRotation(display.getRotation() ^ 1);
   }
-
-  Wire.begin(SDA, SCL, 400000);
-
-  sensor.begin();
 }
 
 void loop(void)
@@ -27,4 +29,6 @@ void loop(void)
 
   display.setCursor(display.width()/2 - 64, display.height()/2);
   display.printf("Temp:%4.2f ", temperature);
+
+  ESP_LOGI("loop", "%4.2f", temperature);
 }
